942ef1f50a0841e6f2e0f46ab310df6ee78e06f3
colin11702 <colinmcherney@gmail.com>
2025-04-21 18:10:30 -0700

    cleaned unused files

--- saint-central/app/(tabs)/ministry-chat/utils/cacheUtils.ts
+++
@@ -1,7 +0,0 @@
-// AsyncStorage keys for caching
-export const MEMBERSHIP_CACHE_KEY = (ministryId: number, userId: string) =>
-  `ministry_membership_${ministryId}_${userId}`;
-
-export const MINISTRY_CACHE_KEY = (ministryId: number) => `ministry_details_${ministryId}`;
-
-export const MESSAGES_CACHE_KEY = (ministryId: number) => `ministry_messages_${ministryId}`;
